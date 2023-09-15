5d78b94c760a51de9b1df1d7d5d4be2d6bcc9555
JOAO MEIRELES <128010364+JoaoBonfimMeireles@users.noreply.github.com>
2023-09-15 15:57:42 -0300

    add img

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import { nbaTeams } from "./data/nbaTeams";
 
 import "./App.css";
+import Content from "./componentes/Content/Content";
 
 function App() {
   const [search, setSearch] = useState("");
   const [searchClick, setSearchClick] = useState("");
-
 
   console.log(search);
 
@@ -32,30 +32,35 @@ function App() {
   };
 
   console.log(searchClick);
-  
-  const hasContent = teams.some((team) => team.name === search || team.name === searchClick);
+
+  const hasContent = teams.some(
+    (team) => team.name === search || team.name === searchClick
+  );
 
   return (
     <div className="App">
-      <input
-        type="search"
-        placeholder="Digite o nome ou cidade"
-        value={searchClick || search}
-        onChange={handleInputChange}
-      />
+      <div className="box-input">
+        <input
+          type="search"
+          placeholder="Digite o nome ou cidade"
+          value={searchClick || search}
+          onChange={handleInputChange}
+        />
 
-      <ul className={`${!hasContent ? "ul-clean " : "ul-dity"}`}>
-        {teams.map((team) => (
-          <li key={team.name} onClick={() => handleClick(team.name)}>
-            <img
-              className="logo-search"
-              src={team.logoImgLink}
-              alt={team.name}
-            />
-            <p>{team.name}</p>
-          </li>
-        ))}
-      </ul>
+        <ul className={`${!hasContent ? "ul-clean " : "ul-dity"}`}>
+          {teams.map((team) => (
+            <li key={team.name} onClick={() => handleClick(team.name)}>
+              <img
+                className="logo-search"
+                src={team.logoImgLink}
+                alt={team.name}
+              />
+              <p>{team.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Content />
     </div>
   );
 }
---
+++ src/componentes/Content/Content.tsx
@@ -0,0 +1,12 @@
+export default function Content() {
+  return (
+    <div>
+      <div className="box-top">
+        <img src="https://raw.githubusercontent.com/JoaoBonfimMeireles/nba-filter/main/public/assets/lakers.png" alt="" />{" "}
+        <span>Lakers</span>
+      </div>
+
+      
+    </div>
+  );
+}
